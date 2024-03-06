2fc439e580abf6cdc2c16d0191a0f31651f27d0a
SrJaimito <jaime.senors@gmail.com>
2024-03-06 08:06:49 +0100

    Implement ldhl_sp_e

--- src/gbc/core/instructions.rs
+++ src/gbc/core/instructions.rs
@@ -1,9 +1,15 @@
 use super::Core;
-use super::register_file::{SingleReg, DoubleReg, map_3bit_field};
+use super::register_file::{SingleReg, DoubleReg, map_3bit_field, Flag};
 
 pub struct InstructionInfo(u8, u8);
 
 impl Core {
+
+    ///////////////////////////////////
+    //                               //
+    //  8-Bit Transfer Instructions  //
+    //                               //
+    ///////////////////////////////////
 
     fn ld_r8_r8(&mut self, opcode: u8) -> InstructionInfo {
         let src_reg = match map_3bit_field(opcode & 0x07) {
@@ -211,6 +217,124 @@ impl Core {
         self.reg.write_dreg(DoubleReg::HL, addr - 1);
 
         InstructionInfo(1, 8)
+    }
+
+    ////////////////////////////////////
+    //                                //
+    //  16-Bit Transfer Instructions  //
+    //                                //
+    ////////////////////////////////////
+
+    fn ld_r16_n16(&mut self, opcode: u8) -> InstructionInfo {
+        let lsb = self.mem.read(self.pc + 1) as u16;
+        let msb = self.mem.read(self.pc + 2) as u16;
+
+        let value = (msb << 8) | lsb;
+
+        let masked_opcode = (opcode >> 4) & 0x03;
+
+        if masked_opcode == 0x03 {
+            self.sp = value;
+        } else {
+            let dst_reg = match masked_opcode {
+                0x00 => DoubleReg::BC,
+                0x01 => DoubleReg::DE,
+                0x02 => DoubleReg::HL,
+                _ => panic!("Error dst_reg ld_r16_n16")
+            };
+
+            self.reg.write_dreg(dst_reg, value);
+        }
+
+        InstructionInfo(3, 12)
+    }
+
+    fn ld_sp_hl(&mut self) -> InstructionInfo {
+        let value = self.reg.read_dreg(DoubleReg::HL);
+        self.sp = value;
+
+        InstructionInfo(1, 8)
+    }
+
+    fn push_qq(&mut self, opcode: u8) -> InstructionInfo {
+        match (opcode >> 4) & 0x03 {
+            0x00 => {
+                self.mem.write(self.sp - 1, self.reg.read_reg(SingleReg::B));
+                self.mem.write(self.sp - 2, self.reg.read_reg(SingleReg::C));
+            },
+            0x01 => {
+                self.mem.write(self.sp - 1, self.reg.read_reg(SingleReg::D));
+                self.mem.write(self.sp - 2, self.reg.read_reg(SingleReg::E));
+            },
+            0x02 => {
+                self.mem.write(self.sp - 1, self.reg.read_reg(SingleReg::H));
+                self.mem.write(self.sp - 2, self.reg.read_reg(SingleReg::L));
+            },
+            0x03 => {
+                self.mem.write(self.sp - 1, self.reg.read_reg(SingleReg::A));
+                self.mem.write(self.sp - 2, self.reg.read_reg(SingleReg::F));
+            },
+            _ => panic!("Error src_reg push_qq")
+        }
+
+        self.sp -= 2;
+
+        InstructionInfo(1, 16)
+    }
+
+    fn pop_qq(&mut self, opcode: u8) -> InstructionInfo {
+        match (opcode >> 4) & 0x03 {
+            0x00 => {
+                self.mem.write(self.sp, self.reg.read_reg(SingleReg::C));
+                self.mem.write(self.sp + 1, self.reg.read_reg(SingleReg::B));
+            },
+            0x01 => {
+                self.mem.write(self.sp, self.reg.read_reg(SingleReg::E));
+                self.mem.write(self.sp + 1, self.reg.read_reg(SingleReg::D));
+            },
+            0x02 => {
+                self.mem.write(self.sp, self.reg.read_reg(SingleReg::L));
+                self.mem.write(self.sp + 1, self.reg.read_reg(SingleReg::H));
+            },
+            0x03 => {
+                self.mem.write(self.sp, self.reg.read_reg(SingleReg::F));
+                self.mem.write(self.sp + 1, self.reg.read_reg(SingleReg::A));
+            },
+            _ => panic!("Error src_reg push_qq")
+        }
+
+        self.sp += 2;
+
+        InstructionInfo(1, 12)
+    }
+
+    fn ldhl_sp_e(&mut self) -> InstructionInfo {
+        let sp = self.sp as i32;
+        let e = (self.mem.read(self.pc + 1) as i8) as i32);
+        
+        let result = sp + e;
+        let carry = sp ^ e ^ result;
+
+        let h = if (carry & 0x0000_1000) != 0 {
+            true
+        } else {
+            false
+        };
+
+        let cy = if (carry & 0x0001_0000) != 0 {
+            true
+        } else {
+            false
+        };
+
+        self.reg.write_dreg(DoubleReg::HL, result as u16);
+
+        self.reg.set_flag(Flag::Z, false);
+        self.reg.set_flag(Flag::N, false);
+        self.reg.set_flag(Flag::H, h);
+        self.reg.set_flag(Flag::CY, cy);
+
+        InstructionInfo(2, 12)
     }
 
 }
